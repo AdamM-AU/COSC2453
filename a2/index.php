767c32fb333cdc8758ff72570fd806fa2d558839
AdamM-AU <70795583+AdamM-AU@users.noreply.github.com>
2022-07-08 16:36:27 +1000

    A2: More Corrections
    - Applied more formatting corrections
    - Fixed bad practices used in supplied template

--- a2/index.php
+++ a2/index.php
@@ -1,9 +1,12 @@
 <?PHP
     /* 
-	 * Assignment 2 - By Adam Mutimer (S3875753)
+	 * Assignment 2 - By Adam Mutimer (s3875753)
 	 * 	https://github.com/AdamM-AU
 	 *
 	*/
+	
+	// Its good practice to set the timezone when using date functions, also prevents php warnings - Adam Mutimer
+	date_default_timezone_set("Australia/Melbourne");
 ?>
 <!DOCTYPE html>
 <html lang='en'>
@@ -14,7 +17,7 @@
     
 		<!-- Keep wireframe.css for debugging, add your css to style.css -->
 		<link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
-		<link id='stylecss' type="text/css" rel="stylesheet" href="style.css?t=<?= filemtime("style.css"); ?>">
+		<link id='stylecss' type="text/css" rel="stylesheet" href="style.css?t=<?PHP echo filemtime("style.css"); // BAD PRACTICE: Not all systems allow PHP Short Tags "<? =" - Adam Mutimer ?>">
 		<script src='../wireframe.js'></script>
 	</head>
 
@@ -36,12 +39,21 @@
 		</main>
 
 		<footer>
-			<div>&copy;
+			<div>Copytight&copy; <?PHP echo date("Y"); // Use Servers Year for copyright ?>
+			<!--
+				// THIS IS BAD PRACTICE TO RELY ON THE DATE ON THE CLIENTS MACHINE - Adam Mutimer
 			<script>
-				document.write(new Date().getFullYear()); <!-- PHP Alternative: <?PHP echo date("Y"); ?>
-			</script> Put your name(s), student number(s) and group name here. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
-			<div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
-			<div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
+				document.write(new Date().getFullYear()); <!-- PHP Alternative: 
+			</script> 
+			-->
+			- Adam Mutimer (s3875753). Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.
+			</div>
+			<div>
+				Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.
+			</div>
+			<div>
+				<button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button>
+			</div>
 		</footer>
 		
 	</body>
