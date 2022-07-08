f9168bac37240d7ee9f57d4dbc1021a8b09840c0
AdamM-AU <70795583+AdamM-AU@users.noreply.github.com>
2022-07-09 02:54:28 +1000

    A2: FINAL BUG FIX FOR Titan(rmit)

--- a2/functions.php
+++ a2/functions.php
@@ -12,7 +12,7 @@ function loadCSVArray( $filename ) {
 	while ( !feof($csv) ) { // Read $csv and check for "end of file (EOF)" until we get  "end of file (EOF)"
 		$row = fgetcsv($csv, 1000, ','); // Read each line of CSV into array $csvRows[], accept no more than 1000 lines, comma delimited
 		
-		if ($row[0] !== NULL) { // Skip empty/blank rows
+		if (!empty($row[0]) && !is_null($row[0])) { // Skip empty/blank rows
 			$csvRows[] = $row;
 		}
 	}
