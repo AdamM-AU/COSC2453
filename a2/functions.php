db3620e29e4f6814110fff558b5488f7b05634f9
AdamM-AU <70795583+AdamM-AU@users.noreply.github.com>
2022-07-08 17:10:33 +1000

    A2 - loadCSVArray()
    We now skip empty/blank lines

--- a2/functions.php
+++ a2/functions.php
@@ -10,13 +10,15 @@ function loadCSVArray( $filename ) {
 	$csv = fopen($filename, 'r'); // Open file "read only"
 	
 	while ( !feof($csv) ) { // Read $csv and check for "end of file (EOF)" until we get  "end of file (EOF)"
-		$csvRows[] = fgetcsv($csv, 1000, ','); // Read each line of CSV into array $csvRows[], comma delimited, accept no more than 1000 lines
+		$row = fgetcsv($csv, 1000, ','); // Read each line of CSV into array $csvRows[], comma delimited, accept no more than 1000 lines
+		
+		if ($row[0] !== NULL) { // Skip empty/blank rows
+			$csvRows[] = $row;
+		}
 	}
 	
 	fclose ($csv); // Close the file/file pointer
 	return $csvRows; // Return the array to the calling module
 }
-
-print_r(loadCSVArray("movies.csv"));
 	 
 ?>
