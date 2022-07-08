58bc913e9ca394a1945eb81b3800bc6697676b8b
AdamM-AU <70795583+AdamM-AU@users.noreply.github.com>
2022-07-09 02:47:18 +1000

    A2: Final Submission

--- a2/booking.php
+++ a2/booking.php
@@ -42,35 +42,219 @@
 		<main>
 
 			<section class="movie" id="movie">
-				<div class="container">				
+				<div class="container center">
 					<?PHP
 					// Change Content based on 3 DIGIT code supplied in URL
-					if ($validCode = checkMovieCode($_GET['code']) == TRUE) {
-						$movie = getMovieByCode($_GET['code']);
+					$movieCode = htmlspecialchars(pg_escape_string($_GET['code'])); // Sanitize
+					
+					if ($validCode = checkMovieCode($movieCode) == TRUE) {
+						$movie = getMovieByCode($movieCode);
 						echo "<h1 class=\"center booking-movie-title\">$movie[0]</h1>\n";
-						echo "\t\t\t\t\t" . "<h3 class=\"center\">Rating: $movie[6]</h3>\n";
+						echo "\t\t\t\t\t" . "<h3>Rating: $movie[6]</h3>\n";
 						echo "\t\t\t\t\t" . '<iframe style="display:block;" width="560" height="315" src="' . $movie[8] . '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' . "\n";
-						echo "\t\t\t\t\t" . "<p class=\"center\">$movie[9]</p>\n";						
+						echo "\t\t\t\t\t" . "<p>$movie[9]</p><br>\n";					
+						echo "\t\t\t\t\t" . "<h3>The Cast:</h3>\n";
+						echo "\t\t\t\t\t" . "<p>$movie[10], $movie[11], $movie[12]</p><br>\n";
 						
 					} else {
 						echo "<h1 class=\"center\">ERROR: MOVIE CODE INVALID!</h1>";
 					}
 					?>
-				</div>
-			</section>
 				<?PHP
 					// If Valid Code display this html
 					if ($validCode) {
 				?>
-			<section class="booking" id="booking">
-				<div class="container">				
-					// Booking Form
-				</div>
-			</section>					
+					<h2>Booking Information</h2>
+					<table class="booking-table">
+					<form name="booking" method="post" action="">
+						<input type="hidden" name="movie" value="<?PHP echo $movieCode; ?>" readonly>
+						<tr>
+							<td><label class = 'booking-label' for='STA'>Standard Adult</label></td>
+							<td>
+								<select id="STA" name="seats[STA]" data-full="20.50" data-disc="15.00">
+									<option disabled selected>Select Required Tickets</option>
+									<option value="1">1</option>
+									<option value="2">2</option>
+									<option value="3">3</option>
+									<option value="4">4</option>
+									<option value="5">5</option>
+									<option value="6">6</option>
+									<option value="7">7</option>
+									<option value="8">8</option>
+									<option value="9">9</option>
+									<option value="10">10</option>
+								</select>
+							</td>
+						</tr>
+
+						<tr>
+							<td><label class="booking-label" for="STP">Standard Concession</label></td>
+							<td>
+								<select id="STP" name="seats[STP]" data-full="18.00" data-disc="13.50">
+									<option disabled selected>Select Required Tickets</option>
+									<option value="1">1</option>
+									<option value="2">2</option>
+									<option value="3">3</option>
+									<option value="4">4</option>
+									<option value="5">5</option>
+									<option value="6">6</option>
+									<option value="7">7</option>
+									<option value="8">8</option>
+									<option value="9">9</option>
+									<option value="10">10</option>
+								</select>
+							</td>
+						</tr>
+
+						<tr>
+							<td><label class="booking-label" for="STC">Standard Child</label></td>
+							<td>
+								<select id="STC" name="seats[STC]" data-full="16.50" data-disc="12.00">
+									<option disabled selected>Select Required Tickets</option>
+									<option value="1">1</option>
+									<option value="2">2</option>
+									<option value="3">3</option>
+									<option value="4">4</option>
+									<option value="5">5</option>
+									<option value="6">6</option>
+									<option value="7">7</option>
+									<option value="8">8</option>
+									<option value="9">9</option>
+									<option value="10">10</option>
+								</select>
+							</td>
+						</tr>
+						
+						<tr>
+							<td><label class="booking-label" for="FCA">First Class Adult</label></td>
+							<td>
+								<select id="FCA" name="seats[FCA]" data-full="30.00" data-disc="24.00">
+									<option disabled selected>Select Required Tickets</option>
+									<option value="1">1</option>
+									<option value="2">2</option>
+									<option value="3">3</option>
+									<option value="4">4</option>
+									<option value="5">5</option>
+									<option value="6">6</option>
+									<option value="7">7</option>
+									<option value="8">8</option>
+									<option value="9">9</option>
+									<option value="10">10</option>
+								</select>
+							</td>
+						</tr>
+						
+						<tr>
+							<td><label class="booking-label" for="FCP">First Class Concession</label></td>
+							<td>
+								<select id="FCP" name="seats[FCP]" data-full="27.00" data-disc="22.50">
+									<option disabled selected>Select Required Tickets</option>
+									<option value="1">1</option>
+									<option value="2">2</option>
+									<option value="3">3</option>
+									<option value="4">4</option>
+									<option value="5">5</option>
+									<option value="6">6</option>
+									<option value="7">7</option>
+									<option value="8">8</option>
+									<option value="9">9</option>
+									<option value="10">10</option>
+								</select>
+							</td>
+						</tr>
+						
+						<tr>
+							<td><label class="booking-label" for="FCC">First Class Child</label></td>
+							<td>
+								<select id="FCC" name="seats[FCC]" data-full="24.00" data-disc="21.00">
+									<option disabled selected>Select Required Tickets</option>
+									<option value="1">1</option>
+									<option value="2">2</option>
+									<option value="3">3</option>
+									<option value="4">4</option>
+									<option value="5">5</option>
+									<option value="6">6</option>
+									<option value="7">7</option>
+									<option value="8">8</option>
+									<option value="9">9</option>
+									<option value="10">10</option>
+								</select>
+							</td>
+						</tr>
+						</table>
+						
+						<hr>
+						<h3>Day &amp; Time</h3>
+						<?PHP
+							// Monday & Tuesday
+							if (!empty($movie[2])) {
+								echo "<span class=\"radio-btn\">";
+								echo "\t<input type=\"radio\" id=\"MON\" name=\"day\" value=\"MON\" data-pricing=\"discprice\">";
+								echo "\t<label for=\"MON\">Monday: $movie[2]</label>";
+								echo "</span>";
+								
+								echo "<span class=\"radio-btn\">";
+								echo "\t<input type=\"radio\" id=\"TUE\" name=\"day\" value=\"TUE\" data-pricing=\"discprice\">";
+								echo "\t<label for=\"TUE\">Tuesday: $movie[2]</label>";
+								echo "</span>";
+							}
+							
+							// Wednesday - Friday
+							if (!empty($movie[3])) {
+								echo "<span class=\"radio-btn\">";
+								echo "\t<input type=\"radio\" id=\"WED\" name=\"day\" value=\"WED\" data-pricing=\"discprice\">";
+								echo "\t<label for=\"WED\">Wednesday: $movie[3]</label>";
+								echo "</span>";
+								
+								echo "<span class=\"radio-btn\">";
+								echo "\t<input type=\"radio\" id=\"THU\" name=\"day\" value=\"THU\" data-pricing=\"discprice\">";
+								echo "\t<label for=\"THU\">Thursday: $movie[3]</label>";
+								echo "</span>";
+								
+								echo "<span class=\"radio-btn\">";
+								echo "\t<input type=\"radio\" id=\"FRI\" name=\"day\" value=\"FRI\" data-pricing=\"discprice\">";
+								echo "\t<label for=\"FRI\">Friday: $movie[3]</label>";
+								echo "</span>";
+							}
+
+							// Saturday & Sunday
+							if (!empty($movie[4])) {
+								echo "<span class=\"radio-btn\">";
+								echo "\t<input type=\"radio\" id=\"SAT\" name=\"day\" value=\"SAT\" data-pricing=\"fullprice\">";
+								echo "\t<label for=\"SAT\">Saturday: $movie[4]</label>";
+								echo "</span>";
+								
+								echo "<span class=\"radio-btn\">";
+								echo "\t<input type=\"radio\" id=\"SUN\" name=\"day\" value=\"SYN\" data-pricing=\"fullprice\">";
+								echo "\t<label for=\"SUN\">Sunday: $movie[4]</label>";
+								echo "</span>";
+							}							
+							
+						?>
+						<br>
+						<br>
+						<hr>
+						
+						<h3>Your Details</h3>
+						<label class="booking-label" for="name">Full Name:</label><br>
+						<input type="text" id="name" name="user[name]" placeholder="John Snow" required><br>
+						
+						<label class="booklabel" for="email">Email:</label><br>
+						<input type="email" id="email" name="user[email]email" placeholder="YOU@SOMEWHERE.COM" required><br>
+						
+						<label class="booklabel" for="mobile">Mobile Number:</label><br>
+						<input type="tel" id="mobile" name="user[mobile]"  pattern="[0-9]{10}" placeholder="04XXXXXXXX" required><br>
+						
+						<br>
+						<br>
+						<button class="btn btn-primary" type="submit">Create Booking</button
+					</form>
 				<?PHP
 				}
 				?>
-				
+
+				</div>
+			</section>
 		</main>
 		
 		<footer>
