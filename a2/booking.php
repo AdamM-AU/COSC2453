1d46b0eb67d76a227ef9545ea4afb3f643af8141
AdamM-AU <70795583+AdamM-AU@users.noreply.github.com>
2022-07-08 17:15:59 +1000

    A2: booking.php
    Fixed my mistake indenting <pre> contents... oops

--- a2/booking.php
+++ a2/booking.php
@@ -63,12 +63,12 @@
 			<hr>
 			<h3>Debug Area</h3>
 			<pre>
-				GET Contains:
-				<?php print_r($_GET) ?>
-				POST Contains:
-				<?php print_r($_POST) ?>
-				SESSION Contains:
-				<?php print_r($_SESSION) ?>
+GET Contains:
+<?php print_r($_GET) ?>
+POST Contains:
+<?php print_r($_POST) ?>
+SESSION Contains:
+<?php print_r($_SESSION) ?>
 			</pre>
 		</aside>
 
