4dcf706b4ddf0dd2fd5c145756a768b0a795b868
AdamM-AU <70795583+AdamM-AU@users.noreply.github.com>
2022-07-08 17:17:23 +1000

    A2: functions.php
    Comment order corrections, mostly to make it friendlier for others to understand whats going on here

--- a2/functions.php
+++ a2/functions.php
@@ -10,7 +10,7 @@ function loadCSVArray( $filename ) {
 	$csv = fopen($filename, 'r'); // Open file "read only"
 	
 	while ( !feof($csv) ) { // Read $csv and check for "end of file (EOF)" until we get  "end of file (EOF)"
-		$row = fgetcsv($csv, 1000, ','); // Read each line of CSV into array $csvRows[], comma delimited, accept no more than 1000 lines
+		$row = fgetcsv($csv, 1000, ','); // Read each line of CSV into array $csvRows[], accept no more than 1000 lines, comma delimited
 		
 		if ($row[0] !== NULL) { // Skip empty/blank rows
 			$csvRows[] = $row;
