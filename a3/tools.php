d252b486d5e8ac938ce86b53db2c5f5789da5af2
AdamM-AU <70795583+AdamM-AU@users.noreply.github.com>
2022-07-09 15:54:02 +1000

    A4: Function Descriptions
    Planning out new PHP functions required for assignment 3

--- a3/tools.php
+++ a3/tools.php
@@ -1,9 +1,60 @@
 <?php
-  session_start();
+	session_start(); // Start a session
 
-/* Put your PHP functions and modules here.
-   Many will be provided in the teaching materials,
-   keep a look out for them!
-*/
+	/*	
+	 *	My Projects Functions - By Adam Mutimer (s3875753)
+	 *
+	 *	Function Outline:
+	 *		loadCSVArray( SRING filename ) 		- Opens a CSV and Reads it into an array and returns the array
+	 *		checkMovieCode( STRING movieCode ) 	- Checks MovieCode against the movie csv, returns boolean true or false
+	 *		getMovieByCode( STRING MovieCode ) 	- Returns an array containing only the requested movie
+	 *		createBooking( ARRAY bookingData )	- Creates a booking in the booking spreadsheet, Returns BookingID
+	 *		checkBooking( ARRAY bookingData )	- Checks spreadsheet for bookings matching (BookingID OR Email OR Name OR Mobile)
+	 *		retrieveBooking ( INT bookingID ) 	- Returns array containing booking information
+	 *
+	 */
+
+	// Open CSV and read into an array :)
+	// ADAM: Amend code to support 2D Keyed Array
+	function loadCSVArray( $filename ) {
+		$csv = fopen($filename, 'r'); // Open file "read only"
+		
+		while ( !feof($csv) ) { // Read $csv and check for "end of file (EOF)" until we get  "end of file (EOF)"
+			$row = fgetcsv($csv, 1000, ','); // Read each line of CSV into array $csvRows[], accept no more than 1000 lines, comma delimited
+			
+			if (!empty($row[0]) && !is_null($row[0])) { // Skip empty/blank rows
+				$csvRows[] = $row;
+			}
+		}
+		
+		fclose ($csv); // Close the file/file pointer
+		unset($csv); // Unload from memory, garbage collector will do it when it's ready
+		
+		return $csvRows; // Return the array to the calling module
+	}
+
+	// Return True if valid movie code or False if invalid
+	function checkMovieCode($movieCode) {
+		$array = loadCSVArray( "movies.csv" );
+		
+		foreach ($array as $row) {
+			$movieCodes[] = $row[1];
+		}
+		unset($array); // Unload from memory, garbage collector will do it when it's ready
+		return in_array($movieCode, $movieCodes, false);
+	}
+
+	// Fetch movie by movie code
+	function getMovieByCode($movieCode) {
+		$array = loadCSVArray( "movies.csv" );
+		
+		foreach ($array as $row) {
+			if ($row[1] == $movieCode) {
+				$movie = $row;
+			}
+		}
+		unset($array); // Unload from memory, garbage collector will do it when it's ready
+		return $movie;
+	}
 
 ?>
